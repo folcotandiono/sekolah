50db76a9b931e6abbdf83b140d541f84e61e43d8
folcotandiono <folcotandiono@gmail.com>
2018-05-01 23:34:39 +0700

    .

--- application/controllers/Murid.php
+++ application/controllers/Murid.php
@@ -84,10 +84,46 @@ class Murid extends Rest_Controller {
 		$this->db->select('soal_ujian_detail.id, soal_ujian_detail.id_soal_ujian, soal_ujian_detail.id_jenis_soal_ujian_detail, soal_ujian_detail.soal_tulisan, soal_ujian_detail.soal_gambar, soal_ujian_detail.pilihan_jawaban_tulisan, soal_ujian_detail.pilihan_jawaban_gambar, soal_ujian_detail.kunci_jawaban');
         $this->db->from('soal_ujian_detail');
 		$this->db->where('soal_ujian_detail.id_soal_ujian', $id_soal_ujian);
-        $result = $this->db->get()->result();
+		$result = $this->db->get()->result();
 
-        $message = array("list_soal_ujian_detail"=> $result);
+		$message = array("list_soal_ujian_detail"=> $result);
 
         $this->response($message, 200);
+	}
+
+	public function tambah_jawaban_soal_ujian_detail_post()
+	{
+		$jawaban_soal_ujian_detail['id_soal_ujian_detail'] = $this->post("id_soal_ujian_detail");
+		$jawaban_soal_ujian_detail['id_murid'] = $this->post("id_murid");
+		$jawaban_soal_ujian_detail['id_jadwal_ujian'] = $this->post('id_jadwal_ujian');
+		$jawaban_soal_ujian_detail['jawaban_tulisan'] = $this->post('jawaban_tulisan');
+
+		print_r($jawaban_soal_ujian_detail);
+
+		$this->db->insert('jawaban_soal_ujian_detail', $jawaban_soal_ujian_detail);
+
+        $message = array("list_jawaban_soal_ujian_detail"=> $jawaban_soal_ujian_detail);
+
+        $this->response($message, 200);
+	}
+
+	public function sudah_ujian_get()
+	{
+		$id_soal_ujian = $this->get("id_soal_ujian");
+		$id_murid = $this->get("id_murid");
+		$id_jadwal_ujian = $this->get("id_jadwal_ujian");
+
+		$this->db->select('*');
+		$this->db->from('soal_ujian');
+		$this->db->join('soal_ujian_detail', 'soal_ujian.id = soal_ujian_detail.id_soal_ujian');
+		$this->db->join('jawaban_soal_ujian_detail', 'soal_ujian_detail.id = jawaban_soal_ujian_detail.id_soal_ujian_detail');
+		$this->db->where('soal_ujian.id', $id_soal_ujian);
+		$this->db->where('jawaban_soal_ujian_detail.id_murid', $id_murid);
+		$this->db->where('jawaban_soal_ujian_detail.id_jadwal_ujian', $id_jadwal_ujian);
+		$result = $this->db->get()->result();
+
+		$message = array("banyak" => count($result));
+
+		$this->response($message, 200);
 	}
 }
